067d31743bdbf8956bf2a15b6fc705cc3c7630c9
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-03 18:03:56 +0200

    renamed back and now it works

--- apps/app01/web.py
+++ apps/app01/web.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  version = "1.0alpha"
-  return '[app01 v{}] hello, world from Skaffold in python.'.format(version)
+  #version = "1.0alpha"
+  return '[app01 vTODO] hello, world from Skaffold in python.' # .format(version)
