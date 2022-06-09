0459552edeaf46cee802386fa9a3e2cb3404ef7c
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-09 15:13:04 +0200

    quotes typo

--- cloud-build/01-on-commit-build.sh
+++ cloud-build/01-on-commit-build.sh
@@ -12,9 +12,18 @@ echo "CB_DATETIME:      $$DATE-$$TIME"
 echo "EdwardDATEIME:    $(date +%y%m%d-%s)"
 echo "MagicVersion:     $MAGIC_VERSION"
 echo "SuperMagicVersion: $(cat apps/$_DEPLOY_UNIT/VERSION )"
-gcloud deploy releases
-        create "$_DEPLOY_UNIT-$$DATE-$$TIME-$MAGIC_VERSION" 
+
+gcloud deploy releases create "$_DEPLOY_UNIT-$$DATE-$$TIME-$MAGIC_VERSION" 
         --delivery-pipeline="$_DEPLOY_UNIT"
         --build-artifacts=/workspace/artifacts.json
-        --skaffold-file="apps/$_DEPLOY_UNIT"/skaffold.yaml"
+        --skaffold-file="apps/$_DEPLOY_UNIT/skaffold.yaml"
         --region="${_DEPLOY_REGION}"
+
+
+echo gcloud deploy releases create "$_DEPLOY_UNIT-$$DATE-$$TIME-$MAGIC_VERSION" 
+        --delivery-pipeline="$_DEPLOY_UNIT"
+        --build-artifacts=/workspace/artifacts.json
+        --skaffold-file="apps/$_DEPLOY_UNIT/skaffold.yaml"
+        --region="${_DEPLOY_REGION}"
+
+echo All done.
