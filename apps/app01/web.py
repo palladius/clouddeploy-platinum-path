fb121ee1962e7fb8f3c011a3a5cdf88eaf4088c6
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-09 12:29:25 +0200

    other changes

--- apps/app01/web.py
+++ apps/app01/web.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,5 +13,10 @@ with open("VERSION", "r") as f:
 
 def index():
   version = version_from_file # "1.1a"
+  fav_color = os.environ.get('FAVORITE_COLOR')
+
   print("[ricc][web.py] INDEX: the super-duper vanilla server in python to say HelloWorld v{}!\n".format(version))
-  return "[app01 v<b>{}</b>] Hello world from Skaffold in python!".format(version)
+  return """<h1>app01 v<b>{}</b></h1> 
+        Hello world from Skaffold in python! This is a demonstrative app to demonstrate CI/CD with Cloud Deploy and Cloud Build<br/>
+        FAVORITE_COLOR={}
+  """.format(version, fav_color)
