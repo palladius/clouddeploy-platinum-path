ff0da564d0303b7e05bdce7a8607c6dcddf4a01f
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-09 15:24:34 +0200

    try-again

--- cloud-build/01-on-commit-build.sh
+++ cloud-build/01-on-commit-build.sh
@@ -5,10 +5,13 @@
 
 #  | tr '[:upper:]' '[:lower:]' 
 export MAGIC_VERSION=$(cat VERSION  | tr '[:upper:]' '[:lower:]' )
+# need to pass from CLI since it wont expand within bash :/
+export CLOUDBUILD_DATETIME="$1"
 
 echo "_DEPLOY_UNIT:     $_DEPLOY_UNIT"
 echo "_DEPLOY_REGION:   $_DEPLOY_REGION"
-echo "CB_DATETIME:      $$DATE-$$TIME"
+echo "CB_DATETIME1:      $$DATE-$$TIME"
+echo "CB_DATETIME2:      $CLOUDBUILD_DATETIME"
 echo "EdwardDATEIME:    $(date +%y%m%d-%s)"
 echo "MagicVersion:     $MAGIC_VERSION"
 echo "SuperMagicVersion: $(cat apps/$_DEPLOY_UNIT/VERSION )"
