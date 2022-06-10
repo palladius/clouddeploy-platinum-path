fadc16d6bc4e9f8caf3d60e42648915c86442a48
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-10 11:11:56 +0200

    better versions

--- cloud-build/01-on-commit-build.sh
+++ cloud-build/01-on-commit-build.sh
@@ -4,23 +4,21 @@
 
 
 #  | tr '[:upper:]' '[:lower:]' 
-export MAGIC_VERSION=$(cat VERSION  | tr '[:upper:]' '[:lower:]' )
+export APPROXIMATE_MAGIC_VERSION=$(cat VERSION  | tr '[:upper:]' '[:lower:]' )
+export SUPERDUPER_MAGIC_VERSION=$(cat "apps/$ARGV_DEPLOY_UNIT/VERSION" )
 # need to pass from CLI since it wont expand within bash :/
-export CLOUDBUILD_DATETIME="$1"
 
 ARGV_DEPLOY_UNIT="$1"
 ARGV_DEPLOY_REGION="$2" # deploy region
 ARGV_DATETIME="$3"
 
 BASH_DATETIME=$(date +%Y%m%d-%H%M)
+
 # These dont work: https://screenshot.googleplex.com/ABKSubdGMi99Xy6
-#echo "_DEPLOY_UNIT:     $_DEPLOY_UNIT"
-#echo "_DEPLOY_REGION:   $_DEPLOY_REGION"
-echo "CB_DATETIME1:     $$DATE-$$TIME"
-echo "CB_DATETIME2:     $CLOUDBUILD_DATETIME"
+echo "CB_DATE/TIME:     $DATE-$TIME"
 echo "BASH_DATETIME:    $BASH_DATETIME"             # Edward: +%y%m%d-%s but i dont like it
-echo "MAGIC_VERSION:    $MAGIC_VERSION"    # fake version, local to here and not from a certain app
-echo "SuperMagicVersion: $(cat "apps/$ARGV_DEPLOY_UNIT/VERSION" )" # The REAL thing, scrapes for VERSION number in proper apps/$MYAPP/VERSION :)
+echo "APPROXIMATE_MAGIC_VERSION: $APPROXIMATE_MAGIC_VERSION"    # fake version, local to here and not from a certain app
+echo "SUPERDUPER_MAGIC_VERSION:  $SUPERDUPER_MAGIC_VERSION" # The REAL thing, scrapes for VERSION number in proper apps/$MYAPP/VERSION :)
 echo "ARGV1:            $1" # 1. ARGV_DEPLOY_UNIT, eg 'app02'
 echo "ARGV2:            $2" # 2. ARGV_DEPLOY_REGION, eg 'europe-west1'
 echo "ARGV3:            $3" # 3. ARGV_DATETIME - useless, eg '$DATE-£TIME' - useless
@@ -29,12 +27,16 @@ echo "CBENV_BUILD_ID:   $CBENV_BUILD_ID"
 echo "PROJECT_ID:       $PROJECT_ID"
 echo "PROJECT_NUMBER:   $PROJECT_NUMBER"
 echo "REV:              $REV"
-echo "CBENV_DATETIME1:  $CBENV_DATETIME1"
-echo "CBENV_DATETIME2:  $CBENV_DATETIME2"
+# This dont work:
+#echo "CBENV_DATETIME1:  $CBENV_DATETIME1"
+#echo "CBENV_DATETIME2:  $CBENV_DATETIME2"
+echo "CBENV_DATETIME3:  $CBENV_DATETIME3"
+#echo "_DEPLOY_UNIT:     $_DEPLOY_UNIT"
+#echo "_DEPLOY_REGION:   $_DEPLOY_REGION"
 
 set -x 
 
-gcloud deploy releases create "$ARGV_DEPLOY_UNIT-$BASH_DATETIME-$SuperMagicVersion" \
+gcloud deploy releases create "a$ARGV_DEPLOY_UNIT-b$BASH_DATETIME-c$SUPERDUPER_MAGIC_VERSION" \
         --delivery-pipeline="$ARGV_DEPLOY_UNIT" \
         --build-artifacts=/workspace/artifacts.json \
         --skaffold-file="apps/$ARGV_DEPLOY_UNIT/skaffold.yaml" \
