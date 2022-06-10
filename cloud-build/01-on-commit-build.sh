d285890c1cdb292052a852ef6a9f321959b32f77
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-10 11:32:53 +0200

    smallfix

--- cloud-build/01-on-commit-build.sh
+++ cloud-build/01-on-commit-build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # This will be called
-
+ARGV_DEPLOY_UNIT="$1"
+ARGV_DEPLOY_REGION="$2" # deploy region
+ARGV_DATETIME="$3"
+ARGV_DATETIME2="$4"
 
 #  | tr '[:upper:]' '[:lower:]' 
 export APPROXIMATE_MAGIC_VERSION=$(cat VERSION  | tr '[:upper:]' '[:lower:]' )
 export SUPERDUPER_MAGIC_VERSION=$(cat "apps/$ARGV_DEPLOY_UNIT/VERSION" )
 # need to pass from CLI since it wont expand within bash :/
 
-ARGV_DEPLOY_UNIT="$1"
-ARGV_DEPLOY_REGION="$2" # deploy region
-ARGV_DATETIME="$3"
-ARGV_DATETIME2="$4"
 
 BASH_DATETIME=$(date +%Y%m%d-%H%M)
 
@@ -37,7 +36,7 @@ echo "REV:              $REV"
 
 set -x 
 
-gcloud deploy releases create "a$ARGV_DEPLOY_UNIT-b$BASH_DATETIME-c$SUPERDUPER_MAGIC_VERSION" \
+gcloud deploy releases create "$ARGV_DEPLOY_UNIT-$BASH_DATETIME-$SUPERDUPER_MAGIC_VERSION" \
         --delivery-pipeline="$ARGV_DEPLOY_UNIT" \
         --build-artifacts=/workspace/artifacts.json \
         --skaffold-file="apps/$ARGV_DEPLOY_UNIT/skaffold.yaml" \
