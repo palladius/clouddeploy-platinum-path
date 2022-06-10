29b3f4a97c1cc87d5d98070d46c05f64e607d42b
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-10 10:13:42 +0200

    removing project number as CB doesnt know it (wOOOt)

--- cloud-build/01-on-commit-build.sh
+++ cloud-build/01-on-commit-build.sh
@@ -8,6 +8,10 @@ export MAGIC_VERSION=$(cat VERSION  | tr '[:upper:]' '[:lower:]' )
 # need to pass from CLI since it wont expand within bash :/
 export CLOUDBUILD_DATETIME="$1"
 
+ARGV_DEPLOY_UNIT="$1"
+ARGV_REGION="$2"
+ARGV_DATETIME="$3"
+
 echo "_DEPLOY_UNIT:     $_DEPLOY_UNIT"
 echo "_DEPLOY_REGION:   $_DEPLOY_REGION"
 echo "CB_DATETIME1:      $$DATE-$$TIME"
@@ -24,7 +28,8 @@ echo "PROJECT_ID:       $PROJECT_ID"
 echo "PROJECT_NUMBER:   $PROJECT_NUMBER"
 echo "REV:              $REV"
 echo "CBENV_DATETIME:   $CBENV_DATETIME"
-gcloud deploy releases create "$_DEPLOY_UNIT-$$DATE-$$TIME-$MAGIC_VERSION" \
+
+gcloud deploy releases create "$_DEPLOY_UNIT-$CBENV_DATETIME-$MAGIC_VERSION" \
         --delivery-pipeline="$_DEPLOY_UNIT" \
         --build-artifacts=/workspace/artifacts.json \
         --skaffold-file="apps/$_DEPLOY_UNIT/skaffold.yaml" \
