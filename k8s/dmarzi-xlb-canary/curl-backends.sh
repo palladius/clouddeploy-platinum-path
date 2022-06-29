63c81e6362e755689f0dbd521cd46c96cac358da
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-29 16:45:43 +0200

    renaming

--- k8s/dmarzi-xlb-canary/curl-backends.sh
+++ k8s/dmarzi-xlb-canary/curl-backends.sh
@@ -14,12 +14,15 @@ set -x
 # curl -H "host: store.example.io" "http://${IP}/canary/" 2>/dev/null
 
 echo "2. curling Ricc Services:"
-RICC_SERVICE_IP="$(gcloud compute forwarding-rules list | grep gkegw  | grep ricc-external-store | awk '{print $2}' )"
+#RICC_SERVICE_IP="$(gcloud compute forwarding-rules list | grep gkegw  | grep ricc-external-store | awk '{print $2}' )"
 
 # This works for Carlesso only:
-dns-setup-palladius.sh ricc-store.palladius.it "$RICC_SERVICE_IP"
+#dns-setup-palladius.sh ricc-store.palladius.it "$RICC_SERVICE_IP"
 # gcloud --quiet --project ric-cccwiki beta dns record-sets create --rrdatas='34.117.142.130' --type=A --ttl=300 --zone=palladius-it ricc-store.palladius.it
-curl -H "host: ricc-store.palladius.it" "http://$RICC_SERVICE_IP/"
+#curl -H "host: ricc-store.palladius.it" "http://$RICC_SERVICE_IP/"
+#curl http://ricc-store.palladius.it 2>/dev/null
+curl http://ricc-store.palladius.it/storev1 2>/dev/null
+curl http://ricc-store.palladius.it/storev2 2>/dev/null
 
 echo THE END.
 
