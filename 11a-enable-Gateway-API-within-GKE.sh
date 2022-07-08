9216393aa8f5cecc53bb958abd3ad99ed4dd0865
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-07-08 17:15:43 +0200

    renaming

--- 11a-enable-Gateway-API-within-GKE.sh
+++ 11a-enable-Gateway-API-within-GKE.sh
@@ -48,15 +48,15 @@ gcloud container fleet memberships register $CLUSTER_2 \
 # Cluster 1
 echo lets recall that: CLUSTER_1="$CLUSTER_1"
 echo lets recall that: CLUSTER_2="$CLUSTER_2"
-#yellow "Try now for cluster1=$CLUSTER_1 kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster1/"
+#yellow "Try now for cluster1=$CLUSTER_1 kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster1/"
 
 gcloud container clusters get-credentials "$CLUSTER_1" --region "$GCLOUD_REGION" --project "$PROJECT_ID"
 kubectl config get-contexts
-kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster1/
+kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster1/
 
 gcloud container clusters get-credentials "$CLUSTER_2" --region "$GCLOUD_REGION" --project "$PROJECT_ID"
 kubectl config get-contexts
-kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster2/
+kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster2/
 
 #3. enable multi-cluster services
 gcloud container fleet multi-cluster-services enable \
--- 11b-kubectl-apply-stuff.sh
+++ 11b-kubectl-apply-stuff.sh
@@ -9,7 +9,7 @@ set -e
 # Cluster 1
 yellow Lets recall that: CLUSTER_1="$CLUSTER_1"
 yellow Lets recall that: CLUSTER_2="$CLUSTER_2"
-#yellow "Try now for cluster1=$CLUSTER_1 kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster1/"
+#yellow "Try now for cluster1=$CLUSTER_1 kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster1/"
 
 kubectl config get-contexts
 
@@ -17,11 +17,11 @@ kubectl config get-contexts
 set -x
 
 gcloud container clusters get-credentials "$CLUSTER_1" --region "$GCLOUD_REGION" --project "$PROJECT_ID"
-kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster1/
+kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster1/
 
 gcloud container clusters get-credentials "$CLUSTER_2" --region "$GCLOUD_REGION" --project "$PROJECT_ID"
 #kubectl config get-contexts
-kubectl apply -f  $GKE_SOLUTION1_SETUP_DIR/cluster2/
+kubectl apply -f  $GKE_SOLUTION_ILB_SETUP_DIR/cluster2/
 
 echo Restoring cluster 1.
 gcloud container clusters get-credentials "$CLUSTER_1"  --region "$GCLOUD_REGION" --project "$PROJECT_ID"
