363db687ae0c3f184bf868de5676b2f1063d6f83
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-07-11 11:39:33 +0200

    done

--- 06-cloud-build-locally.sh
+++ 06-cloud-build-locally.sh
@@ -20,8 +20,10 @@ which cloud-build-local >/dev/null  &&
 PARTICULAR_VERSION_FOR_MODULE=$(cat apps/$MODULE_TO_BUILD/VERSION )
 
 gcloud auth configure-docker $REGION-docker.pkg.dev
+# documented herE: https://cloud.google.com/artifact-registry/docs/docker/pushing-and-pulling#auth
+#which docker-credential-gcloud  && docker-credential-gcloud configure-docker #  $REGION-docker.pkg.dev
 
-cloud-build-local --config="cloudbuild.yaml" --dryrun=false \
+_echodo cloud-build-local --config="cloudbuild.yaml" --dryrun=false \
   --substitutions "_DEPLOY_UNIT=$MODULE_TO_BUILD,_REGION=$REGION,_ARTIFACT_REPONAME=$ARTIFACT_REPONAME,_DEPLOY_REGION=$CLOUD_DEPLOY_REGION" \
   --push apps/app01/
 
