d13e48fb4cd662685f8f85e654c7366953621c19
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-07-11 14:19:49 +0200

    minor

--- 12-canary-step-by-step.sh
+++ 12-canary-step-by-step.sh
@@ -8,12 +8,10 @@ set -e
 # Add your code here:
 kubectl config get-contexts
 
-#DEV="gke_${PROJECT_ID}_europe-west6_cicd-dev"
-#CAN="gke_${PROJECT_ID}_europe-west6_cicd-canary"
-#PRD="gke_${PROJECT_ID}_europe-west6_cicd-prod"
-
+# Example west/east
 #kubectl apply --context gke-west-1 -f https://raw.githubusercontent.com/GoogleCloudPlatform/gke-networking-recipes/main/gateway/gke-gateway-controller/multi-cluster-gateway/store-west-1-service.yaml
 #kubectl apply --context gke-west-2 -f https://raw.githubusercontent.com/GoogleCloudPlatform/gke-networking-recipes/main/gateway/gke-gateway-controller/multi-cluster-gateway/store-west-2-service.yaml
+
 kubectl apply --context "$GKE_CANARY_CLUSTER_CONTEXT" 
 
 #######################
