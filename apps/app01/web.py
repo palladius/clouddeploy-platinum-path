7e7bd36bf71c3defb031e3fb6485dd226866d016
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-07-11 11:42:39 +0200

    better app paragraphing, less arrogance

--- apps/app01/web.py
+++ apps/app01/web.py
@@ -17,10 +17,14 @@ def index():
 
   print("[ricc][web.py] INDEX: the super-duper vanilla server in python to say HelloWorld - v{}!\n".format(version))
   return """<h1>App01 (🐍) v<b>{ver}</b></h1> 
+
         Hello world from Skaffold in python! This is a demonstrative app to demonstrate CI/CD with Cloud Deploy and Cloud Build<br/>
-        Im so clever that now I read version from file and this ./VERSION file is actually read by the build pipeline
-        into the Cloud Deploy release name - wOOOt!
-        FAVORITE_COLOR={fav_color}
+
+        I read version from file and this ./VERSION file is actually read by the build pipeline
+        into the Cloud Deploy release name - wOOOt!<br/>
+        
+        FAVORITE_COLOR={fav_color}<br/>
+        
         Favorite Color from v14: <b style='background-color:#{fav_color};' >#{fav_color}</b><br/>
 
         <hr/>
