1730cd5aef1235024dd34ba4fb683c6655b86965
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-07-13 14:17:37 +0200

    ported everything to the template folder

--- 12-solution1-gateway-API.sh
+++ 12-solution1-gateway-API.sh
@@ -14,29 +14,32 @@ APP_NAME="${1:-$DEFAULT_APP}"
 
 #kubectl config get-contexts | grep cicd | grep "$PROJECT_ID" | grep "$REGION"
 
-# Example west/east
-#kubectl apply --context gke-west-1 -f https://raw.githubusercontent.com/GoogleCloudPlatform/gke-networking-recipes/main/gateway/gke-gateway-controller/multi-cluster-gateway/store-west-1-service.yaml
-#kubectl apply --context gke-west-2 -f https://raw.githubusercontent.com/GoogleCloudPlatform/gke-networking-recipes/main/gateway/gke-gateway-controller/multi-cluster-gateway/store-west-2-service.yaml
-
 #kubectl apply --context "$GKE_CANARY_CLUSTER_CONTEXT" 
 white "Usage (as I want it): $0 <APPNAME> <..>" 
-red TODO ricc implement this for App01/02.
+red Dear friends and colleagues this is STILL WIP. Ricc is implementing this for App01/02 since the POC works for a generic STORE.
 yellow Try to use app01 with solution01 and make it parametric in ARGV..
 white "Now I proceed to apply solution 1 for: $APP_NAME. If wrong, call me with proper usage."
 
 # ensure out dir exists..
 mkdir -p "$GKE_SOLUTION1_XLB_PODSCALING_SETUP_DIR/out/"
 
+white This grep output should be null:
+grep store $GKE_SOLUTION1_XLB_PODSCALING_SETUP_DIR/templates/*yaml | egrep -v '^#'
+
+###############################################
+# set up additional variables for the for cycle
+REGION="${GCLOUD_REGION}"
+PREFIX="${APP_NAME}-${REGION}" # maybe in the future PREFIX = APP-REGION
+SOLUTION1_TEMPLATING_VER="1-0"
+###############################################
+
 for TEMPLATE_FILE in "$GKE_SOLUTION1_XLB_PODSCALING_SETUP_DIR/templates/"*.template.yaml ; do 
   DEST_FILE=$(echo "$TEMPLATE_FILE" | sed s:/templates/:/out/:) 
-  echo "Hydrating template '$TEMPLATE_FILE' into this tmp out/ file: $DEST_FILE:"
+  echo "Hydrating template '$TEMPLATE_FILE' [v.$SOLUTION1_TEMPLATING_VER] into this tmp out/ file: $DEST_FILE:"
 
-  # set up additional variables
-  PREFIX="${APP_NAME}" # maybe in the future PREFIX = APP-REGION
-  REGION="${GCLOUD_REGION}"
   (
     echo '###########################################################'
-    echo "# Created by $0 on `date` on `hostname`"
+    echo "# Created by $0 v$SOLUTION1_TEMPLATING_VER on `date` on `hostname`"
     echo "# Edit at your own risk as it'll be soon overwritten. Maybe." 
     echo '###########################################################'
     cat "$TEMPLATE_FILE"  | egrep -v '^#'  # remove comments
@@ -44,6 +47,7 @@ for TEMPLATE_FILE in "$GKE_SOLUTION1_XLB_PODSCALING_SETUP_DIR/templates/"*.templ
     sed -e "s/__MY_PROJECT_ID__/$PROJECT_ID/g" |
     sed -e "s/__PREFIX__/$PREFIX/g" |    
     sed -e "s/__APP_NAME__/$APP_NAME/g" |    
+    sed -e "s/__APPNAME__/$APP_NAME/g" |    # I know what you thinking..
     sed -e "s/__MY_REGION__/$REGION/g" |
     sed -e "s/__MY_DOMAIN__/$MY_DOMAIN/g" |
     #sed -e "s/__MY_VERSION_/$CLOUD_DEPLOY_TEMPLATING_VER/g" |
