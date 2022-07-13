23531d918f0fb31e3dae819fc7bd4f1e12230194
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-07-13 12:06:37 +0200

    creating sol1 templates

--- 12-solution1-gateway-API.sh
+++ 12-solution1-gateway-API.sh
@@ -5,6 +5,11 @@ source .env.sh || fatal 'Couldnt source this'
 #set -x
 set -e
 
+DEFAULT_APP="app01"
+APP_NAME="${1:-$DEFAULT_APP}"
+
+K8S_IMAGE="skaf-app01-python-buildpacks"
+K8S_SELECTOR="app: ??"
 # Add your code here:
 
 #kubectl config get-contexts | grep cicd | grep "$PROJECT_ID" | grep "$REGION"
@@ -14,9 +19,10 @@ set -e
 #kubectl apply --context gke-west-2 -f https://raw.githubusercontent.com/GoogleCloudPlatform/gke-networking-recipes/main/gateway/gke-gateway-controller/multi-cluster-gateway/store-west-2-service.yaml
 
 #kubectl apply --context "$GKE_CANARY_CLUSTER_CONTEXT" 
-red TODO ricc 
-yellow Try a usare la app01 con solution01
-white test white removeme
+white "Usage (as I want it): $0 <APPNAME> <..>" 
+red TODO ricc implement this for App01/02.
+yellow Try to use app01 with solution01 and make it parametric in ARGV..
+white "Now I proceed to apply solution 1 for: $APP_NAME. If wrong, call me with proper usage."
 
 #######################
 # End of your code here
