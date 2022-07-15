733639a40b9c232359ee73d630e7e5523efa3d23
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-07-15 16:50:01 +0200

    sol0 seems now COMPLETE!

--- 12-solution0-kubectl-apply-stuff.sh
+++ 12-solution0-kubectl-apply-stuff.sh
@@ -9,9 +9,10 @@ set -e
 ###########################################################
 DEFAULT_APP="app01"                                # app01 / app02
 #DEFAULT_APP_IMAGE="skaf-app01-python-buildpacks"   # skaf-app01-python-buildpacks // ricc-app02-kuruby-skaffold
-APP_NAME="${1:-$DEFAULT_APP}"
-#K8S_APP_IMAGE="${2:-$DEFAULT_APP_IMAGE}" # "skaf-app01-python-buildpacks"
+DEFAULT_APP_SELECTOR="app01-kupython"     # app01-kupython / app02-kuruby
 
+APP_NAME="${1:-$DEFAULT_APP}"
+export K8S_APP_SELECTOR="${2:-$DEFAULT_APP_SELECTOR}"
 ############################################################
 # [RICC01] Troubleshooting info..
 ###########################################################
@@ -19,10 +20,10 @@ APP_NAME="${1:-$DEFAULT_APP}"
 #if /bin/false ; then # restore when part 2 works :) and re_add when its too slow or youre in debug mode.
 
     # Cluster 1
-    yellow Lets recall that: CLUSTER_1="$CLUSTER_1"
-    yellow Lets recall that: CLUSTER_2="$CLUSTER_2"
+    #yellow Lets recall that: CLUSTER_1="$CLUSTER_1"
+    #yellow Lets recall that: CLUSTER_2="$CLUSTER_2"
     #yellow "Try now for cluster1=$CLUSTER_1 kubectl apply -f  $GKE_SOLUTION0_ILB_SETUP_DIR/cluster1/"
-    kubectl config get-contexts
+    #kubectl config get-contexts
     #_kubectl_on_both_canary_and_prod get gatewayclass
     bin/kubectl-triune get gatewayclass
 
@@ -34,6 +35,7 @@ APP_NAME="${1:-$DEFAULT_APP}"
 for CLUSTER in cluster1 cluster2 ; do
     # In case I get it wrong.. :)
     APPNAME="$APP_NAME"
+    APP_SELECTOR="$K8S_APP_SELECTOR"
     # apply smart substitutions..
     smart_apply_k8s_templates_with_envsubst \
         "$GKE_SOLUTION0_ILB_SETUP_DIR/templates/$CLUSTER/" \
@@ -41,9 +43,6 @@ for CLUSTER in cluster1 cluster2 ; do
 done
 #envsubst
 
-echo TODO kubectl apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/"
-kubectl --context="$GKE_CANARY_CLUSTER_CONTEXT" apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/cluster1/"
-kubectl --context="$GKE_PROD_CLUSTER_CONTEXT"   apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/cluster2/"
 
 red "WARNING: The ILB selector just uses canary-or-prod, not appname. Once it works and OVER reaches I can then specify. This fix is for another day."
 white "Solution0 - related endpoints:"
@@ -55,8 +54,12 @@ set -x
 # [RICC03] APPLY
 ###########################################################
 # Step 6. Apply the GW Config on Cluster 1.
-kubectl --context=$GKE_CANARY_CLUSTER_CONTEXT apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/cluster1/"
-kubectl --context=$GKE_PROD_CLUSTER_CONTEXT   apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/cluster2/"
+# this is the STATIC by Daniel, not the multitenant from jul15: #MultiAppK8sRefactoring
+# kubectl --context=$GKE_CANARY_CLUSTER_CONTEXT apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/cluster1/"
+# kubectl --context=$GKE_PROD_CLUSTER_CONTEXT   apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/cluster2/"
+#echo TODO kubectl apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/"
+kubectl --context="$GKE_CANARY_CLUSTER_CONTEXT" apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/cluster1/"
+kubectl --context="$GKE_PROD_CLUSTER_CONTEXT"   apply -f "$GKE_SOLUTION0_ILB_SETUP_DIR/out/cluster2/"
 
 echo Restoring cluster 1.
 gcloud container clusters get-credentials "$CLUSTER_1"  --region "$GCLOUD_REGION" --project "$PROJECT_ID"
